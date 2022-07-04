1d5c04f23aee8687e1a61c5ae94e85618c4ca22c
ltrodrig <lucastomas.rodriguezdoval@everis.nttdata.com>
2022-07-04 12:52:39 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import productRouter from './router/productRouter.js'
+import cartRouter from './router/cartRouter.js'
+import {} from 'dotenv/config'
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+
+app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
+
